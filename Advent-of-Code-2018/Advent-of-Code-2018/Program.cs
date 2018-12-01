1e379910c59d16f45dbc5278e8c74f5d344a8bba
dcondamines <dcondamines@outlook.com>
2018-12-01 16:02:58 -0500

    Initial Release

---
+++ Advent-of-Code-2018/Advent-of-Code-2018/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code_2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArray = File.ReadAllLines(path: @"D:\Data\Documents\Advent of Code 2018\Day01.txt");
+            int frequency = 0;
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                char operation = inputArray[i].First();
+                int change = Int32.Parse(inputArray[i].Substring(1));
+
+                switch (operation)
+                {
+                    case '+':
+                        frequency += change;
+                        break;
+
+                    case '-':
+                        frequency -= change;
+                        break;
+
+                    default:
+                        break;
+
+                }
+            }
+            Console.WriteLine("The resulting frequency after all the changes is " + frequency);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Advent-of-Code-2018/Day-01-Exercise-02/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_01_Exercise_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArray = File.ReadAllLines(path: @"D:\Data\Documents\Advent of Code 2018\Day01.txt");
+            int frequency = 0;
+
+            List<int> frequenciesReached = new List<int>();
+            bool found = false;
+
+            while (!found)
+            {
+                foreach (string input in inputArray)
+                {
+                    char operation = input.First();
+                    int change = Int32.Parse(input.Substring(1));
+
+                    switch (operation)
+                    {
+                        case '+':
+                            frequency += change;
+                            break;
+
+                        case '-':
+                            frequency -= change;
+                            break;
+
+                        default:
+                            break;
+
+                    }
+
+                    if (frequenciesReached.Contains(frequency))
+                    {
+                        found = true;
+                        break;
+                    }
+                    else
+                    {
+                        frequenciesReached.Add(frequency);
+                    }
+                }
+            }
+
+            Console.WriteLine("The first frequency reached twice is " + frequency);
+            Console.ReadKey();
+        }
+    }
+}
