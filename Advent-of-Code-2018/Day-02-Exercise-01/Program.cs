397bffd17689e09d8b9dd77b1908026fa1380f2f
dcondamines <dcondamines@outlook.com>
2018-12-02 15:01:34 -0500

    Day 2 Initial Release

---
+++ Advent-of-Code-2018/Day-02-Exercise-01/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_02_Exercise_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArray = File.ReadAllLines(path: @"D:\Data\Documents\Advent of Code 2018\Day02.txt");
+
+            int twoCount = 0;
+            int threeCount = 0;
+            int checksum = 0;
+
+            foreach (string input in inputArray)
+            {
+                Dictionary<char, int> letterCount = new Dictionary<char, int>();
+
+                foreach (char letter in input)
+                {
+                    if (!letterCount.ContainsKey(letter))
+                    {
+                        letterCount.Add(letter, 1);
+                    }
+                    else
+                    {
+                        letterCount[letter]++;
+                    }
+                }
+
+                if (letterCount.ContainsValue(2))
+                    twoCount++;
+                if (letterCount.ContainsValue(3))
+                    threeCount++;
+            }
+
+            checksum = twoCount * threeCount;
+
+            Console.WriteLine("The checksum is " + checksum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Advent-of-Code-2018/Day-02-Exercise-02/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_02_Exercise_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputArray = File.ReadAllLines(path: @"D:\Data\Documents\Advent of Code 2018\Day02.txt").ToList();
+            int inputLength = inputArray.First().Length - 1;
+            bool found = false;
+            StringBuilder commonLetters = new StringBuilder(inputLength);
+
+            while(!found)
+            {
+                string inputOne = inputArray.First();
+                inputArray.Remove(inputArray.First());
+
+                foreach (string inputTwo in inputArray)
+                {
+                    commonLetters.Clear();
+                    for (int i = 0; i < inputTwo.Length; i++)
+                    {
+                        if(inputOne[i] == inputTwo[i])
+                        {
+                            commonLetters.Append(inputOne[i]);
+                        }
+                    }
+
+                    if(commonLetters.Length == inputLength)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine("The common letters between the two correct IDs are : " + commonLetters);
+            Console.ReadKey();
+        }
+    }
+}
